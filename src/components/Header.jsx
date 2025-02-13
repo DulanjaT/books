141797934231049c98e933660e4a43a8c43b67e3
DulanjaT <dulanja.coding@gmail.com>
2025-02-13 05:55:33 +0200

    Added comments on functions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,41 @@
-import { Link } from 'react-router-dom';
-import { Button, Box, AppBar, Toolbar, Typography } from '@mui/material';
+import { Link } from "react-router-dom";
+import { Button, Box, AppBar, Toolbar, Typography } from "@mui/material";
 
+/**
+ * This function defines the `Header` component, which serves as the navigation bar for the application.
+ * It includes links to the home page and the "Add New" page using Material-UI components for a clean and modern design.
+ */
 function Header() {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        <Typography
-                            variant="h5"
-                            noWrap
-                            component={Link}
-                            to="/"
-                            sx={{
-                                mr: 2,
-                                color: 'inherit',
-                                textDecoration: 'none',
-                            }}
-                        >
-                            Books
-                        </Typography>
-                    </Typography>
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Typography
+              variant="h5"
+              noWrap
+              component={Link}
+              to="/"
+              sx={{
+                mr: 2,
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              Books
+            </Typography>
+          </Typography>
 
-                    <Button color="inherit" variant="text" component={Link} to="/">
-                        Home
-                    </Button>
-                    <Button color="inherit" variant="text" component={Link} to="/addnew">
-                        Add New
-                    </Button>
-                </Toolbar>
-            </AppBar>
-        </Box>
-    );
+          <Button color="inherit" variant="text" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" variant="text" component={Link} to="/addnew">
+            Add New
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
 export default Header;
--- src/routes/Books.jsx
+++ src/routes/Books.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import useAxios from '../services/useAxios';
+import { useEffect, useState } from "react";
+import useAxios from "../services/useAxios";
 import {
   Box,
   Card,
@@ -12,106 +12,118 @@ import {
   Chip,
   Typography,
   Grid,
-} from '@mui/material';
-import Search from '../components/Search';
-import { Link } from 'react-router-dom';
+} from "@mui/material";
+import Search from "../components/Search";
+import { Link } from "react-router-dom";
 
-
+/**
+ * The `Books` component fetches and displays a list of books.
+ * It also provides a search functionality to filter the displayed books.
+ */
 function Books() {
   const [books, setBooks] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const { data, alert, loading, get, post, update, remove } = useAxios(
-    'http://localhost:3000'
+    "http://localhost:3000"
   );
 
-
   useEffect(() => {
+    /**
+     * Fetches the list of books from the server when the component mounts
+     * if the books list is currently empty.
+     */
     if (books?.length === 0) {
       getBooks();
     }
   }, []);
 
   useEffect(() => {
-    if(data != undefined)
-      setBooks(data);
+    /**
+     * Updates the `books` state whenever the fetched data changes.
+     */
+    if (data != undefined) setBooks(data);
   }, [data]);
 
+  /**
+   * Fetches the list of books from the server.
+   */
   async function getBooks() {
-    get('books');
+    get("books");
   }
 
   return (
-    <Box sx={{ mx: 'auto', p: 2 }}>
+    <Box sx={{ mx: "auto", p: 2 }}>
       {loading && <CircularProgress />}
       {!loading && (
         <div>
-          <Grid container spacing={2}>  
-            <Grid item xs={12} sm={12} justifyContent={'center'}>
-              <Search updateBooks={(books) => {
-                if(data != undefined)
-                  setBooks(books);
-              }}/>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={12} justifyContent={"center"}>
+              <Search
+                updateBooks={(books) => {
+                  if (data != undefined) setBooks(books);
+                }}
+              />
             </Grid>
-            <Grid item xs={12} sm={12} justifyContent={'center'}>
-            <Stack
-              sx={{ justifyContent: 'space-around' }}
-              spacing={{ xs: 1 }}
-              direction="row"
-              useFlexGap
-              flexWrap="wrap"
-            >
-              {books?.map((book) => (
-                <Card
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    width: '15%',
-                    minWidth: 200,
-                  }}
-                  key={book.name}
-                  to={"/book/" + book.id}
-                  component={Link}
-                >
-                  <CardMedia
-                    sx={{ height: 250 }}
-                    image={book.img}
-                    title={book.name}
-                  />
-                  <Box sx={{ pt: 2, pl: 2 }}>
-                    {book.genres.map((genre, i) => (
-                      <Chip
-                        key={i}
-                        label={genre}
-                        variant="outlined"
+            <Grid item xs={12} sm={12} justifyContent={"center"}>
+              <Stack
+                sx={{ justifyContent: "space-around" }}
+                spacing={{ xs: 1 }}
+                direction="row"
+                useFlexGap
+                flexWrap="wrap"
+              >
+                {books?.map((book) => (
+                  <Card
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      width: "15%",
+                      minWidth: 200,
+                    }}
+                    key={book.name}
+                    to={"/book/" + book.id}
+                    component={Link}
+                  >
+                    <CardMedia
+                      sx={{ height: 250 }}
+                      image={book.img}
+                      title={book.name}
+                    />
+                    <Box sx={{ pt: 2, pl: 2 }}>
+                      {book.genres.map((genre, i) => (
+                        <Chip
+                          key={i}
+                          label={genre}
+                          variant="outlined"
+                          size="small"
+                        />
+                      ))}
+                      <Typography variant="h6" component="h2" sx={{ mt: 2 }}>
+                        {book.name}
+                      </Typography>
+                      <Typography variant="subtitle1" gutterBottom>
+                        {book.author}
+                      </Typography>
+                    </Box>
+                    <CardActions
+                      sx={{
+                        justifyContent: "space-between",
+                        mt: "auto",
+                        pl: 2,
+                      }}
+                    >
+                      <Rating
+                        name="read-only"
+                        value={book.stars}
+                        readOnly
                         size="small"
                       />
-                    ))}
-                    <Typography variant="h6" component="h2" sx={{ mt: 2 }}>
-                      {book.name}
-                    </Typography>
-                    <Typography variant="subtitle1" gutterBottom>
-                      {book.author}
-                    </Typography>
-                  </Box>
-                  <CardActions
-                    sx={{
-                      justifyContent: 'space-between',
-                      mt: 'auto',
-                      pl: 2,
-                    }}
-                  >
-                    <Rating
-                      name="read-only"
-                      value={book.stars}
-                      readOnly
-                      size="small"
-                    />
-                    <Button size="small">Learn More</Button>
-                  </CardActions>
-                </Card>
-              ))}
-            </Stack>
+                      <Button size="small">Learn More</Button>
+                    </CardActions>
+                  </Card>
+                ))}
+              </Stack>
             </Grid>
           </Grid>
         </div>
